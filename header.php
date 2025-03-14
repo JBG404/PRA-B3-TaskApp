3b38d0ed458a5270b2c5aad2eeab059461bb3f29
Aceist <79134787+Aceistt@users.noreply.github.com>
2025-03-14 10:19:09 +0100

    rip diddy
    and lowtaperfade is dead

--- header.php
+++ header.php
@@ -16,5 +16,10 @@ session_start();
         <div class="login">
             <a href="login.php" class="login-btn">Login</a>
         </div>
+        
     </div>
+    <pre style="border: 1px dashed lightgrey; padding: 10px; font-size: 20px;"><?php 
+        if(isset($_SESSION)) print_r($_SESSION);
+        else echo '$_SESSION bestaat niet';
+        ?></pre>
 </header>
