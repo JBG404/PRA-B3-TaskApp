6904d264f0592e6d55c4a1fde701501b34d966fa
Aceist <79134787+Aceistt@users.noreply.github.com>
2025-03-14 10:13:45 +0100

    low taper fade

--- index.php
+++ index.php
@@ -4,5 +4,9 @@ require_once 'head.php';
 <?php
 require_once 'header.php';
 ?>
-        <h1>Welcome to TaskApp</h1>
-        <p class="lead">This is a simple task management application.</p>
+ <div class="banner">
+            <div class="banner-img">
+                <img src="img/logo-big-fill-only.png" alt="placeholder" height="200" width="200">
+                <h2>UNDER CONSTRUCTION GANG</h2>
+            </div>
+        </div>
