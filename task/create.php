03cd5fa67045efb1da95504067320870418c08ae
jbgamingnl <jake@jbgaming.nl>
2025-04-03 12:14:49 +0200

    fixed create.php and index.php

--- task/create.php
+++ task/create.php
@@ -1,52 +1,57 @@
-
 <head>
     <title>Taskapp / Tasks / Create</title>
-    <?php require_once '../head.php'; ?>
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../css/login.css">
+</head>
 </head>
 <?php
 session_start();
-if(!isset($_SESSION['user_id'])){
-    $msg="Jemoeteerstinloggen!";
+if (!isset($_SESSION['user_id'])) {
+    $msg = "Jemoeteerstinloggen!";
     header("Location:../login/login.php?msg=$msg");
     exit;
 } //login heebeejeebies i dont understand
 ?>
+
 <body>
-        <!-- headuh -->
+    <!-- headuh -->
     <?php require_once '../header.php'; ?>
 
     <div class="container">
         <h1>Create Task</h1>
         <!-- FORMETH MAKETH -->
         <form action="../backend/Taskcontroller.php" method="POST">
-        <input type="hidden" name="action" value="create">
-                <!-- sets task name -->
-             <div class="form-group">
+            <input type="hidden" name="action" value="create">
+            <!-- sets task name -->
+            <div class="form-group">
                 <label for="taskname">Task Name:</label>
                 <input type="text" name="taskname" id="taskname">
-            </div><div class="form-group">
-                <!-- sets description -->
-                <label for="description">description:</label>
-                <textarea type="text"name="description" id="description" line=50 rows=10>
             </div>
             <div class="form-group">
-                <!-- sets creator -->
-            <label for="creator">creator:</label>
-            <input type="text" name="creator" id="creator">
+                <!-- sets description -->
+                <label for="description">description:</label>
+                <textarea type="text" name="description" id="description" line=50 rows=10></textarea>
+            </div>
+            <div class="form-group">
+                    <!-- sets creator -->
+                <label for="creator">creator:</label>
+                <input type="text" name="creator" id="creator">
             </div>
             <div class="form-group">
                 <!-- sets worker -->
-            <label for="worker">current worker(s):</label>
-            <input type="text" name="worker" id="worker">
+                <label for="worker">current worker(s):</label>
+                <input type="text" name="worker" id="worker">
             </div>
             <div class="form-group">
                 <!-- sets status (OMG FINALLY NOT A TEXT) -->
-            <label for="status">status:</label>
-            <select name="status" id="status">
-                <option value="todo">to do</option>
-                <option value="inprogress">in progress</option>
-                <option value="done">done</option>
-            </select>
+                <label for="status">status:</label>
+                <select name="status" id="status">
+                    <option value="todo">to do</option>
+                    <option value="inprogress">in progress</option>
+                    <option value="done">done</option>
+                </select>
             </div>
             <!-- actually adds the thingy -->
             <input type="submit" value="Add Task">
--- task/index.php
+++ task/index.php
@@ -6,3 +6,15 @@ if(!isset($_SESSION['user_id'])){
     exit;
 } //login heebeejeebies i dont understand
 ?>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../css/login.css">
+    <title>Taskapp / Tasks </title>
+</head>
+<body>
+    <div class="wrapper">
+        <?php require_once '../header.php'; ?>
+    </div>
+</body>
