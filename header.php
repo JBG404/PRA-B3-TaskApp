c63cb52c9eac11d74a459d134b22a035f36be9e7
IdkMyNaam <127683838+IdkMyNaam@users.noreply.github.com>
2025-04-03 11:17:25 +0200

    header fix

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
     <div class="header-container">
         <!-- Left side: Logo and Navigation -->
         <div class="logo-nav">
-            <img src="img/logo-big-v4.png" alt="Developer Land" height="75" width="190" class="logo">
+            <img src="<?php $base_url ?>/img/logo-big-v4.png" alt="Developer Land" height="75" width="190" class="logo">
             <nav>
                 <ul>
                     <li><a href="<?php $base_url ?>index.php">Home</a></li>
@@ -17,8 +17,8 @@
         </div>
         
     </div>
-    <pre style="border: 1px dashed lightgrey; padding: 10px; font-size: 20px;"><?php 
+    <!-- <pre style="border: 1px dashed lightgrey; padding: 10px; font-size: 20px;"><?php 
         if(isset($_SESSION)) print_r($_SESSION);
         else echo '$_SESSION bestaat niet';
-        ?></pre>
+        ?></pre> -->
 </header>
